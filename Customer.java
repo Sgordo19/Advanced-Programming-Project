c39d94488b52627bfc3d05e53b297e6f6d19a7b6
jadeivory <jadeivory555@gmail.com>
2025-11-21 20:27:19 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,82 +1,58 @@
 package Project;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Customer extends User
 {
-	private String address;
-	private String phoneNumber;
-	private List<String> shipments; // To store shipments IDs
-	
-	
-	// Primary Constructor
-	public Customer (int userID, String firstName, String lastName, String email, String password, String address, String phoneNumber)
-	{
-		super(userID, firstName, lastName, email, password, "Customer");
-		this.address = address;
-		this.phoneNumber = phoneNumber;
-		this.shipments = new ArrayList<>();
-	}
+    private String address;
+    private String phoneNumber;
 
+    // Primary Constructor
+    public Customer(int userID, String firstName, String lastName, String email, String password, String address, String phoneNumber)
+    {
+        super(userID, firstName, lastName, email, password, "Customer");
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
 
-	//Getters and Setters
-	public String getAddress() 
-	{
-		return address;
-	}
+    // Getters and Setters
+    public String getAddress() 
+    {
+        return address;
+    }
 
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
 
-	public void setAddress(String address)
-	{
-		this.address = address;
-	}
+    public String getPhoneNumber() 
+    {
+        return phoneNumber;
+    }
 
+    public void setPhoneNumber(String phoneNumber) 
+    {
+        this.phoneNumber = phoneNumber;
+    }
 
-	public String getPhoneNumber() 
-	{
-		return phoneNumber;
-	}
+    // Functional Methods 
+    public void createShipment(String shipmentDetails)
+    {
+    	// Call ShipmentDAO to create shipment in the database
+        System.out.println("Shipment created for " + firstName + " " + lastName + ": " + shipmentDetails);
+    }
 
+    public void trackShipment(String trackingNumber)
+    {
+        System.out.println(firstName + " " + lastName + " is tracking shipment: " + trackingNumber);
+    }
 
-	public void setPhoneNumber(String phoneNumber) 
-	{
-		this.phoneNumber = phoneNumber;
-	}
+    public void viewInvoice(int invoiceID)
+    {
+        System.out.println("Viewing invoice #" + invoiceID + " for " + firstName);
+    }
 
-
-	public List<String> getShipments() 
-	{
-		return shipments;
-	}
-
-
-	public void setShipments(List<String> shipments) 
-	{
-		this.shipments = shipments;
-	}
-	
-	// Functional Methods 
-	
-	public void createShipment(String shipmentDetails)
-	{
-		shipments.add(shipmentDetails);
-		System.out.println("Shipment created for " + firstName + "" + lastName +":" + shipmentDetails);
-	}
-	
-	public void trackShipment(String trackingNumber)
-	{
-		System.out.println(firstName + " " + lastName + " is tracking shipment: " + trackingNumber);
-
-	}
-	
-	public void viewInvoice (int invoiceID)
-	{
-		System.out.println("Viewing invoice #" + invoiceID + " for " + firstName);
-	}
-	
-	public void makePayment(double amount)
-	{
-		System.out.println(firstName + "" + lastName + "make a payment of $" + amount);
-	}
+    public void makePayment(double amount)
+    {
+        System.out.println(firstName + " " + lastName + " made a payment of $" + amount);
+    }
 }
