91edaf5e0ea4ef99332abc84fcc701c06509319f
Angel Pinnock <121143881+AngelArtyz@users.noreply.github.com>
2025-11-21 21:31:33 -0500

    Update

--- Manager.java
+++ Manager.java
@@ -10,11 +10,33 @@ public class Manager extends User
 
     // Functional Methods 
 
-    public void generateReport(String type, String dateRange)
-    {
-    	// Get generate report from  DB system 
-        System.out.println("Generated " + type + " report for " + dateRange);
-    }
+   // Generate and export report to PDF
+	public void generateAndExportReport(String type, Date from, Date to, String pdfPath) {
+		ReportService service = new ReportService();
+		Report report = null;
+		switch (type.toLowerCase()) {
+			case "shipment":
+				report = service.generateShipmentReport(from, to);
+				break;
+			case "revenue":
+				report = service.generateRevenueReport(from, to);
+				break;
+			case "delivery":
+				report = service.generateDeliveryPerformanceReport(from, to);
+				break;
+			case "vehicle":
+				report = service.generateVehicleUtilizationReport(from, to);
+				break;
+			default:
+				System.out.println("Unknown report type: " + type);
+				return;
+		}
+		if (report != null) {
+			reports.add("Generated " + type + " report for: " + from + " to " + to);
+			report.printReport();
+			report.exportToPDF(pdfPath);
+		}
+	}
 
     public void viewVehicleUtilization()
     {
