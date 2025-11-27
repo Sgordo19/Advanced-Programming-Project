730816d6d64397b949b89522cccd41e888176490
Kenrick14 <kenricktb@gmail.com>
2025-11-26 20:52:18 -0500

    Eclipse

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -9,10 +9,8 @@ public class Main {
 		new SignupView();
 		Shipment s = new Shipment();
 		new ShipmentView(s);
-		
 		Customer c = new Customer();
 		new CustomerDashboard(c);
-		
 		Manager m = new Manager();		
 		new ReportGUI(m);
 		*/
