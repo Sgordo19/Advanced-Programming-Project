bcbb321656ed648c2efb4c403350400ff0047d6c
Kenrick14 <kenricktb@gmail.com>
2025-11-27 18:35:05 -0500

    Added payment function

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		new SignupView();
 		Shipment s = new Shipment();
 		new ShipmentView(s);
+		
 		Customer c = new Customer();
 		new CustomerDashboard(c);
 		Manager m = new Manager();		
